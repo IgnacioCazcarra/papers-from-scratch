a0d69dcbfed9be48c4eef4e11ca8aeee4ddd33ec
Ignacio Cazcarra <nacho.eze.cazcarra@gmail.com>
2023-06-07 22:39:13 -0300

    Naive approach to the model

---
+++ VGG/model.py
@@ -0,0 +1,80 @@
+from torch import nn 
+import torch.nn.functional as F
+
+
+class VGG19(nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        
+        self.pool = nn.MaxPool2d(2, stride=2)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+                
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv8 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+
+        self.conv9 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.conv10 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv11 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv12 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        
+        self.conv13 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv14 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv15 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.conv16 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(512*7*7, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 1000)
+
+        self.apply(self.init_weights)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+                
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv8(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv9(x))
+        x = F.relu(self.conv10(x))
+        x = F.relu(self.conv11(x))
+        x = F.relu(self.conv12(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv13(x))
+        x = F.relu(self.conv14(x))
+        x = F.relu(self.conv15(x))
+        x = F.relu(self.conv16(x))
+        x = self.pool(x)
+
+        x = self.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+
+        return F.softmax(x)
+
+
+    def init_weights(self, m):
+        nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0)
